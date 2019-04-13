512b6b6a714fbddc4ebf764dcfe9c8cbe7894b05
wzyuchen <1101385391@qq.com>
2019-04-13 16:37:59 +0800

    按钮完成

---
+++ Blazui/Blazui.Component/Button/ButtonType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Button
+{
+    public enum ButtonType
+    {
+        /// <summary>
+        /// 普通按钮
+        /// </summary>
+        Button,
+
+        /// <summary>
+        /// 提交按钮
+        /// </summary>
+        Submit,
+
+        /// <summary>
+        /// 重置按钮
+        /// </summary>
+        Reset
+    }
+}
