f43494da6ed427e040d28bc4b703d966cfe33ac1
wzyuchen <1101385391@qq.com>
2019-04-13 18:38:17 +0800

    按钮相关

---
+++ Blazui/Blazui.Component/Button/ButtonSize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Button
+{
+    public enum ButtonSize
+    {
+        /// <summary>
+        /// 大按钮
+        /// </summary>
+        Large,
+
+        /// <summary>
+        /// 默认
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// 小按钮
+        /// </summary>
+        Small,
+
+        /// <summary>
+        /// 迷你按钮
+        /// </summary>
+        XSmall
+    }
+}
