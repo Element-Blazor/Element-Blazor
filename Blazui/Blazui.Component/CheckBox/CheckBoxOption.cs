805556c9c091ea25ef7831065a3c4e5413f44095
wzyuchen <1101385391@qq.com>
2019-04-18 22:30:32 +0800

    CheckBoxGroup

---
+++ Blazui/Blazui.Component/CheckBox/CheckBoxOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.CheckBox
+{
+    public class CheckBoxOption
+    {
+        public bool IsChecked { get; set; }
+        public bool IsDisabled { get; set; }
+        public string Label { get; set; }
+    }
+}
