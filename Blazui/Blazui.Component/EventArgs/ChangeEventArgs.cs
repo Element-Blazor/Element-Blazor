6cb7cc98f887c807a471de20a7ec4e2506d22186
wzyuchen <1101385391@qq.com>
2019-04-20 22:30:51 +0800

    CheckBox

---
+++ Blazui/Blazui.Component/EventArgs/ChangeEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.EventArgs
+{
+    public class ChangeEventArgs<T> : UIChangeEventArgs
+    {
+        public T Target { get; set; }
+    }
+}
