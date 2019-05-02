6d3da81cab8df52eb82ae2e6c1a42aaa62f8dcb9
Charlie <rainhat1@163.com>
2019-05-02 23:18:37 +0800

    ajax 获取razor示例代码

---
+++ Blazui/Blazui.Server/Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+﻿using Blazui.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleController : Controller
+    {
+        [HttpGet("[action]/{name}")]
+        public string Code(string name)
+        {
+            var location = Path.GetDirectoryName(typeof(Startup).Assembly.Location);
+            var razorPath = Path.Combine(location, "Demo", name +".razor");
+
+            var code = System.IO.File.Exists(razorPath)
+                       ?System.IO.File.ReadAllText(razorPath)
+                       :string.Empty;
+            return code;
+        }
+    }
+}
