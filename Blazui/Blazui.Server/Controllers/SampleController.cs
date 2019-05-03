5237d582eba55aad2482f277c1c9490cdba9217c
wzyuchen <1101385391@qq.com>
2019-05-03 17:36:25 +0800

    demo框架

--- Blazui/Blazui.Server/Controllers/SampleController.cs
+++ Blazui/Blazui.Server/Controllers/SampleController.cs
@@ -46,7 +46,9 @@ namespace Blazui.Server.Controllers
                     continue;
                 }
                 demoModel.Type += "." + item.Name;
-                var codeFiles = Directory.EnumerateFiles(Path.Combine(location, item.Name)).OrderBy(x => item.Files.IndexOf(x));
+                var codeFiles = Directory.EnumerateFiles(Path.Combine(location, item.Name))
+                    .Select(Path.GetFileName)
+                    .OrderBy(x => item.Files.IndexOf(x));
                 foreach (var codeFile in codeFiles)
                 {
                     var extension = codeFile.Split('.').LastOrDefault().ToLower();
