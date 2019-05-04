6a7f38c23cbd1ceb4ab6ba860d94112e5a1bf100
wzyuchen <1101385391@qq.com>
2019-05-04 18:16:28 +0800

    BSwitch

---
+++ Blazui/Blazui.Component/Switch/BSwitchBase.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Switch
+{
+    public class BSwitchBase : ComponentBase
+    {
+        [Parameter]
+        public string ActiveValue { get; set; } = "true";
+        [Parameter]
+        public string InactiveValue { get; set; } = "false";
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+        [Parameter]
+        public string ActiveText { get; set; }
+
+        [Parameter]
+        public string InactiveText { get; set; }
+
+        [Parameter]
+        public string ActiveColor { get; set; } = "#409EFF";
+
+        [Parameter]
+        public string InactiveColor { get; set; } = "#C0CCDA";
+        [Parameter]
+        public string Value { get; set; } = "true";
+
+        [Parameter]
+        public EventCallback<UIMouseEventArgs> OnChanged { get; set; }
+
+        public event Func<UIMouseEventArgs, Task> OnChangedAsync;
+        protected async Task OnInternalSwitchChangedAsync(UIMouseEventArgs e)
+        {
+            if (IsDisabled)
+            {
+                return;
+            }
+            if (Value == InactiveValue)
+            {
+                Value = ActiveValue;
+            }
+            else
+            {
+                Value = InactiveValue;
+            }
+            if (OnChanged.HasDelegate)
+            {
+                await OnChanged.InvokeAsync(e);
+            }
+            if (OnChangedAsync != null)
+            {
+                await OnChangedAsync(e);
+            }
+        }
+    }
+}
