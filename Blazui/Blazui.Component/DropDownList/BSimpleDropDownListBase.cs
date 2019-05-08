7edc13e17cd49ef459f9fb406d9d6651830598be
wzyuchen <1101385391@qq.com>
2019-05-08 22:53:21 +0800

    BDropDownList

---
+++ Blazui/Blazui.Component/DropDownList/BSimpleDropDownListBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.DropDownList
+{
+    public class BSimpleDropDownListBase : ComponentBase
+    {
+        [Parameter]
+        public string Placeholder { get; set; } = "请选择";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ Blazui/Blazui.Component/DropDownList/BSimpleDropDownListItemBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.DropDownList
+{
+    public class BDropDownListItemBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter]
+        public BSimpleDropDownList DropDownList { get; set; }
+    }
+}
