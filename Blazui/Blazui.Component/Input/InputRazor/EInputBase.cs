75422d8f7de7f7515848c493b3f611860da2d977
871345188 <871345188@qq.com>
2019-05-09 22:01:07 +0800

    测试

---
+++ Blazui/Blazui.Component/Input/InputRazor/EInputBase.cs
@@ -0,0 +1,22 @@
+﻿using Blazui.Component.Input.Category;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Input.InputRazor
+{
+    public class EInputBase : ComponentBase
+    {
+
+        [Parameter]
+        public InputType IType { get; set; }
+
+        [Parameter]
+        public bool IsDisable { get; set; }
+
+        [Parameter]
+        public bool Maxlength { get; set; }
+    }
+}
