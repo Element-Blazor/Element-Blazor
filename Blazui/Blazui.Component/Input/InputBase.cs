d00edc6ec3c28ededb25b3aa1f9a15b6345d86c8
smith_white <zongqipeng@163.com>
2019-05-13 21:06:38 +0800

    前两个

---
+++ Blazui/Blazui.Component/Input/InputBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Component.Input
+{
+    public class InputBase : ComponentBase
+    {
+        [Parameter]
+        public string InputValue { get; set; }
+
+        [Parameter]
+        public string Placeholder { get; set; } = "请输入内容";
+        [Parameter]
+        public bool IsDisabled { get; set; } = false;
+        [Parameter]
+        public bool IsClearable { get; set; } = false;
+    }
+}
