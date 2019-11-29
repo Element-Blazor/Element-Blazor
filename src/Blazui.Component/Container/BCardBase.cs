bb2afe0e35c81d2edca5ae1f634bf745f57f1e41
wzxinchen <1101385391@qq.com>
2019-11-29 20:37:34 +0800

    Layout 布局面板组件

--- src/Blazui.Component/Container/BCardBase.cs
+++ src/Blazui.Component/Container/BCardBase.cs
@@ -9,9 +9,12 @@ namespace Blazui.Component.Container
     public class BCardBase<TModel> : ComponentBase
     {
         [Parameter]
-        public RenderFragment<TModel> Cls { get; set; }
+        public Func<TModel, string> Cls { get; set; }
         [Parameter]
-        public RenderFragment<TModel> BodyCls { get; set; }
+        public Func<TModel, string> BodyCls { get; set; }
+
+        [Parameter]
+        public Func<TModel, ShadowShowType> Shadow { get; set; }
         [Parameter]
         public IEnumerable<TModel> DataSource { get; set; }
         [Parameter]
--- src/Blazui.Component/Container/BLayoutBase.cs
+++ src/Blazui.Component/Container/BLayoutBase.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace Blazui.Component.Container
 {
     public class BLayoutBase : BComponentBase
     {
+        [Parameter]
+        public bool Fit { get; set; } = true;
+        [Parameter]
+        public float Width { get; set; }
+        [Parameter]
+        public float Height { get; set; }
+
+        [Parameter]
+        public float WestWidth { get; set; } = 200;
+        [Parameter]
+        public float EastWidth { get; set; } = 200;
+        [Parameter]
+        public float NorthHeight { get; set; } = 50;
+        [Parameter]
+        public float SouthWidth { get; set; } = 200;
+        [Parameter]
+        public RenderFragment West { get; set; }
+
+        [Parameter]
+        public RenderFragment East { get; set; }
+
+        [Parameter]
+        public RenderFragment Center { get; set; }
+
+        [Parameter]
+        public RenderFragment North { get; set; }
+
+        [Parameter]
+        public RenderFragment South { get; set; }
+
     }
 }
