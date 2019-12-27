497ebf9b23c37271b383993f705feb88a5dfdf28
wzxinchen <1101385391@qq.com>
2019-12-27 21:29:54 +0800

    https://github.com/wzxinchen/Blazui/issues/31 增加 demo

---
+++ src/Blazui.ServerRender/Demo/Loading/MenuLoadingBase.cs
@@ -0,0 +1,26 @@
+﻿using Blazui.Component;
+using Blazui.Component.Table;
+using Blazui.ServerRender.Demo.Table;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.ServerRender.Demo.Loading
+{
+    public class MenuLoadingBase : ComponentBase
+    {
+        protected IContainerComponent menu;
+
+        protected void ShowLoading(object arg)
+        {
+            menu.Loading();
+        }
+
+        protected void CloseLoading()
+        {
+            menu.Close();
+        }
+    }
+}
