237e6e96b735fb83bebd839130f4983b96fc9be9
wzxinchen <1101385391@qq.com>
2020-01-07 21:36:04 +0800

    增加弹窗关闭方法

--- src/Blazui.Component/BComponentBase.cs
+++ src/Blazui.Component/BComponentBase.cs
@@ -57,6 +57,14 @@ namespace Blazui.Component
         {
             _ = MessageBox.AlertAsync(text);
         }
+        /// <summary>
+        /// 弹出 Confirm 消息
+        /// </summary>
+        /// <param name="text"></param>
+        public async Task<MessageBoxResult> ConfirmAsync(string text)
+        {
+            return await MessageBox.ConfirmAsync(text);
+        }
 
         /// <summary>
         /// 默认情况下所有复杂组件都只进行一次渲染，该方法将组件置为需要再次渲染
