177f3d1cbae4aedbb33ae359fdd38f959fcf738a
wzxinchen <1101385391@qq.com>
2020-01-08 22:11:55 +0800

    Transfer 穿梭组件

--- src/Blazui.Component/BTransferBase.cs
+++ src/Blazui.Component/BTransferBase.cs
@@ -1,4 +1,5 @@
 ﻿using Blazui.Component.CheckBox;
+using Blazui.Component.Form;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Blazui.Component
 {
-    public class BTransferBase : BComponentBase
+    public class BTransferBase : BFieldComponentBase<List<string>>
     {
         private Status list1Status = Status.UnChecked;
         internal Status List1Status
@@ -52,25 +53,75 @@ namespace Blazui.Component
             }
         }
 
+        protected override void FormItem_OnReset(object value, bool requireRerender)
+        {
+            ResetList2(value);
+        }
+
+        private void ResetList2(object value)
+        {
+            var valueList = (List<string>)value;
+            List1.AddRange(List2);
+            if (valueList == null)
+            {
+                List2.Clear();
+            }
+            else
+            {
+                List2 = List1.Where(x => valueList.Contains(x.Id)).ToList();
+                List1.RemoveAll(List2.Contains);
+            }
+            List1Status = Status.UnChecked;
+            List2Status = Status.UnChecked;
+            RequireRender = true;
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            if (FormItem == null)
+            {
+                return;
+            }
+            if (FormItem.OriginValueHasRendered)
+            {
+                return;
+            }
+            FormItem.OriginValueHasRendered = true;
+            if (FormItem.Form.Values.Any())
+            {
+                ResetList2(FormItem.OriginValue);
+            }
+            SyncFieldValue();
+        }
+
         internal void ToLeft()
         {
             List1Checked.Clear();
             list2Status = Status.UnChecked;
             list1Status = Status.UnChecked;
             List1.AddRange(List2Checked);
-            List2.Clear();
+            List2.RemoveAll(List2Checked.Contains);
             List2Checked.Clear();
             RequireRender = true;
+            SyncFieldValue();
         }
+
+        private void SyncFieldValue()
+        {
+            SetFieldValue(List2.Select(x => x.Id).ToList(), true);
+        }
+
         internal void ToRight()
         {
             List2Checked.Clear();
             list1Status = Status.UnChecked;
             list2Status = Status.UnChecked;
             List2.AddRange(List1Checked);
-            List1.Clear();
+            List1.RemoveAll(List1Checked.Contains);
             List1Checked.Clear();
             RequireRender = true;
+            SyncFieldValue();
         }
 
         internal void Status1Changed(Status status, TransferItem transferItem)
