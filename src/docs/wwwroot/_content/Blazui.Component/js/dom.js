66637da00306634120b87097c6678450f2a043a3
wzxinchen <1101385391@qq.com>
2020-04-10 16:40:18 +0800

    publish

---
+++ src/docs/wwwroot/_content/Blazui.Component/js/dom.js
@@ -0,0 +1,307 @@
+﻿window.getChild = function (el, idx) {
+    if (!el) {
+        return;
+    }
+    return el.children[idx];
+};
+window.getClientHeight = function (el) {
+    if (!el) {
+        return this.document.body.clientHeight;
+    }
+    return el.clientHeight || 0;
+};
+window.submitForm = function (el, url) {
+    if (!el) {
+        return;
+    }
+
+    el.method = "post";
+    el.action = url;
+    el.submit();
+};
+window.upload = function (el) {
+    if (!el) {
+        return;
+    }
+    return el.children[1].click();
+};
+
+var uploader, uploadUrl;
+window.clear = function (el) {
+    if (!el) {
+        return;
+    }
+    el.value = null;
+};
+async function executePasteUpload(event) {
+    var items = event.clipboardData && event.clipboardData.items;
+    var files = [];
+    if (items && items.length) {
+        // 检索剪切板items
+        for (var i = items.length - 1; i >= 0; i--) {
+            if (items[i].kind != "file") {
+                continue;
+            }
+            file = items[i].getAsFile();
+            files.push(file);
+            break;
+        }
+    }
+
+    let ids = await convertFiles(files).then(async fs => {
+        return await uploader.invokeMethodAsync("previewFiles", fs);
+    });
+    for (var j = 0; j < files.length; j++) {
+        var result = await new Promise(resolver => {
+            _uploadFile(uploadUrl, files[j], resolver);
+        });
+        await uploader.invokeMethodAsync("fileUploaded", result, ids[j]);
+    }
+    await uploader.invokeMethodAsync("filesUploaded");
+};
+window.registerPasteUpload = function (upload, url) {
+    uploader = upload;
+    uploadUrl = url;
+    document.addEventListener('paste', executePasteUpload);
+};
+window.unRegisterPasteUpload = function () {
+    this.document.removeEventListener("paste");
+};
+function convertFiles(files) {
+    let scanFile = function (file) {
+        return new Promise((resolver) => {
+            let infos = [file.name, file.size.toString()];
+            if (file.type.indexOf("image") != -1) {
+                let img = new Image();
+                img.onload = function () {
+                    infos.push(this.width.toString());
+                    infos.push(this.height.toString());
+                    infos.push(img.src);
+                    resolver(infos);
+                };
+                img.src = URL.createObjectURL(file);
+                return;
+            }
+            resolver(infos);
+        });
+    };
+    let filePromises = [];
+    for (var i = 0; i < files.length; i++) {
+        let file = files.item ? files.item(i) : files[i];
+
+        filePromises.push(new Promise((resolver) => {
+            scanFile(file).then(infos => {
+                resolver(infos);
+            });
+        }));
+    }
+    return Promise.all(filePromises);
+}
+window.scanFiles = function (el) {
+    if (!el) {
+        return [];
+    }
+
+    let files = this.convertFiles(el.files);
+    return files;
+};
+async function _uploadFile(url, file, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) {
+            return;
+        }
+        if (this.status < 200 || this.status >= 300) {
+            return;
+        }
+        let response = JSON.parse(this.responseText);
+        callback([response.code.toString(), response.message || "", response.id, response.url]);
+    };
+    xhr.withCredentials = true;
+    xhr.setRequestHeader("x-requested-with", "XMLHttpRequest");
+    let formData = new this.FormData();
+    formData.append("fileContent", file);
+    xhr.send(formData);
+}
+window.uploadFile = function (el, fileName, url) {
+    return new Promise((resolver, reject) => {
+        let file = null;
+        for (var i = 0; i < el.files.length; i++) {
+            file = el.files[i];
+            if (file.name == fileName) {
+                break;
+            }
+        }
+        _uploadFile(url, file, resolver);
+    });
+    //const temporaryFileReader = new FileReader();
+    //return new Promise((resolve, reject) => {
+    //    temporaryFileReader.onerror = () => {
+    //        temporaryFileReader.abort();
+    //        reject(new DOMException("Problem parsing input file."));
+    //    };
+    //    temporaryFileReader.addEventListener("load", function () {
+    //        var data = [el.value, temporaryFileReader.result.split(',')[1]];
+    //        resolve(data);
+    //    }, false);
+    //    temporaryFileReader.readAsDataURL(el.files[0]);
+    //});
+}
+window.trigger = function (el, eventName) {
+    var eventClass = "";
+
+    // Different events have different event classes.
+    // If this switch statement can't map an eventName to an eventClass,
+    // the event firing is going to fail.
+    switch (eventName) {
+        case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
+        case "mousedown":
+        case "mouseup":
+            eventClass = "MouseEvents";
+            break;
+
+        case "focus":
+        case "change":
+        case "blur":
+        case "select":
+            eventClass = "HTMLEvents";
+            break;
+
+        default:
+            throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
+            break;
+    }
+    let event = this.document.createEvent(eventClass);
+    event.initEvent(eventName);
+    el.dispatchEvent(event);
+}
+window.getClientWidth = function (el) {
+    if (!el) {
+        return this.document.body.clientWidth;
+    }
+    return el.clientWidth || 0;
+};
+window.getOffsetLeft = function (el) {
+    if (!el) {
+        return 0;
+    }
+    return el.offsetLeft || 0;
+};
+window.setDisabled = function (el, disabled) {
+    if (!el) {
+        return;
+    }
+    el.disabled = disabled;
+};
+window.getOffsetTop = function (el) {
+    if (!el) {
+        return 0;
+    }
+    return el.offsetTop || 0;
+};
+window.getPaddingLeft = function (el) {
+    if (!el) {
+        return 0;
+    }
+    return parseInt(getComputedStyle(el, null)["padding-left"]);
+};
+window.getPaddingRight = function (el) {
+    if (!el) {
+        return 0;
+    }
+    return parseInt(getComputedStyle(el, null)["padding-right"]);
+};
+window.disableXScroll = function () {
+    this.document.body.style.overflowX = "hidden";
+}
+window.disableYScroll = function () {
+    this.document.body.style.overflowY = "hidden";
+}
+window.enableXScroll = function () {
+    this.document.body.style.overflowX = "auto";
+}
+window.enableYScroll = function () {
+    this.document.body.style.overflowY = "auto";
+}
+window.removeSelf = function (el) {
+    if (!el) {
+        return;
+    }
+    el.parentNode.removeChild(el);
+};
+window.documentAppendChild = function (el) {
+    if (!el) {
+        return;
+    }
+    document.body.append(el);
+};
+window.elementAppendChild = function (parent, child) {
+    if (!parent || !child) {
+        return 0;
+    }
+    parent.append(child);
+};
+window.getTopRelativeBody = function (el) {
+    if (!el) {
+        return 0;
+    }
+    var bodyRect = document.body.getBoundingClientRect(),
+        elemRect = el.getBoundingClientRect(),
+        offset = elemRect.top - bodyRect.top;
+    return this.parseFloat(offset);
+}
+window.Remove = function (el) {
+    if (!el) {
+        return;
+    }
+    el.remove();
+};
+window.getLeft = function (el) {
+    if (!el) {
+        return 0;
+    }
+    return parseInt(getComputedStyle(el, null)["left"]);
+};
+window.getTop = function (el) {
+    if (!el) {
+        return 0;
+    }
+    return parseInt(getComputedStyle(el, null)["top"]);
+};
+window.getBoundingClientRect = function (el) {
+    if (!el) {
+        return {};
+    }
+    return el.getBoundingClientRect();
+};
+window.setTransform = function (el, value) {
+    if (!el) {
+        return;
+    }
+    el.style.transform = value;
+};
+window.setStyle = function (el, key, value) {
+    if (!el) {
+        return;
+    }
+    el.style[key] = value;
+};
+window.getMarginTop = function (el) {
+    if (!el) {
+        return "";
+    }
+    return el.style["margin-top"];
+};
+window.clearStyle = function (el, key) {
+    if (!el) {
+        return;
+    }
+    el.style[key] = "";
+};
+window.setTransitionAsync = function (el, value) {
+    if (!el) {
+        return;
+    }
+    el.style.transition = value;
+};
---
+++ src/docs/wwwroot/service-worker-assets.js
@@ -0,0 +1,277 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-rldnE7wZYJj3Q43t5v8fg1ojKRwyt0Wtfm+224CacZs=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-xMZ0SaSBYZSHVjFdZTAT\/IjRExRIxSriWcJLcA9nkj0=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-jA4J4h\/k76zVxbFKEaWwFKJccmO0voOQ1DbUW+5YNlI=",
+      "url": "css\/open-iconic\/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-BJ\/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=",
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-+P1oQ5jPzOVJGC52E1oxGXIXxxCyMlqy6A9cNxGYzVk=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-aF5g\/izareSj02F3MPSoTGNbcMBl9nmZKDe04zjU\/ss=",
+      "url": "css\/open-iconic\/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-p\/oxU91iBE+uaDr3kYOyZPuulf4YcPAMNIz6PRA\/tb0=",
+      "url": "css\/open-iconic\/README.md"
+    },
+    {
+      "hash": "sha256-BHNhq10T\/9aejlrFEhS\/NYeYS3Djwv\/pqg2xUjQCx\/s=",
+      "url": "css\/site.css"
+    },
+    {
+      "hash": "sha256-qU+KhVPK6oQw3UyjzAHU4xjRmCj3TLZUU\/+39dni9E0=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-\/JlCd1mca0xft7u4FYPpbLA3zlJ6GkHAuGJSR5NM9iE=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-\/TxhJdPTTLyj2tlWuITpXm8BVEamkbZB71iAuB+mfrQ=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-EjGqqOwFpI2f\/5mZSvoy7Vfu9i2yq\/NxRNSTL0Gx0DE=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-XUgyVPEZdx9Wl1sXIpsC1bP2cMisME4OKlL0Bodfy4U=",
+      "url": "sample-data\/weather.json"
+    },
+    {
+      "hash": "sha256-YMskRevQtmG9\/sULlmIYDhwWCRYGVO+JSW7qruYOJtY=",
+      "url": "_content\/Blazui.Markdown\/css\/editor.css"
+    },
+    {
+      "hash": "sha256-7LPeH0Rdbla9V67fU\/p7Svu40XwKt8y9gblodMkuQ\/4=",
+      "url": "_content\/Blazui.Markdown\/css\/editormd.min.css"
+    },
+    {
+      "hash": "sha256-qtw1gNK2T\/Wn5vFCVYfbTosDPvy\/j1wzLKUqXtWAyHw=",
+      "url": "_content\/Blazui.Markdown\/fonts\/fontawesome-webfont.woff2"
+    },
+    {
+      "hash": "sha256-0SsigNziWbLJKr+AcX7OrtDbWElMfJRe6sGTgr0ooZ8=",
+      "url": "_content\/Blazui.Markdown\/images\/loading.gif"
+    },
+    {
+      "hash": "sha256-bNdfijkO7BEHCMNJqjIbyvs\/eH1TtzXRZlo8Ii65Po4=",
+      "url": "_content\/Blazui.Markdown\/js\/addons.min.js"
+    },
+    {
+      "hash": "sha256-U+YZJx23d8azgeKd5zUzpwLbvI1gksC3S7dyGo+GibU=",
+      "url": "_content\/Blazui.Markdown\/js\/continuelist.js"
+    },
+    {
+      "hash": "sha256-9HHNUGilocW6uSUiYHLiGNXWrnTZGsZLrVeinpgQ7Z0=",
+      "url": "_content\/Blazui.Markdown\/js\/dom.js"
+    },
+    {
+      "hash": "sha256-CxJI28J7LZnG7PQ4fXVEeXQCCwsW+y1C04DLVI4mOwg=",
+      "url": "_content\/Blazui.Markdown\/js\/modes.min.js"
+    },
+    {
+      "hash": "sha256-1mPoPZsExFVBNiblxKve0VYYHZF6dq1zQLiq9FYTVTc=",
+      "url": "_content\/Blazui.Component\/css\/fix.css"
+    },
+    {
+      "hash": "sha256-ow9bO6akiCLq4EHgylQSookSXkumYdBH2uVlrEO0prI=",
+      "url": "_content\/Blazui.Component\/css\/fonts\/element-icons.ttf"
+    },
+    {
+      "hash": "sha256-q0CliXK+LOqzLn412rMTG5WarmODXXvaGnmuUfmnPBc=",
+      "url": "_content\/Blazui.Component\/css\/fonts\/element-icons.woff"
+    },
+    {
+      "hash": "sha256-yVTW9xCJU33Zk7eRRyGSvSDNvinNcd\/rit+Nwk9hED0=",
+      "url": "_content\/Blazui.Component\/css\/index.css"
+    },
+    {
+      "hash": "sha256-4PpNrc2KvPhIrrXTmoD4shdw8\/2crLOdpZFuS5raTvM=",
+      "url": "_content\/Blazui.Component\/js\/dom.js"
+    },
+    {
+      "hash": "sha256-amiQLjWx5S4svtqG0SxR2Fejuecx\/ayets4eD\/DYyTo=",
+      "url": "service-worker.js"
+    },
+    {
+      "hash": "sha256-lZUL1FXd8ei544j0gwwwN+9sfSHGUFCyd\/jfy4OQTmg=",
+      "url": "_framework\/_bin\/Blazui.ClientRender.PWA.dll"
+    },
+    {
+      "hash": "sha256-0uF7O+lQlEMWF50fDLYd6W4wYVkHbGazOQhyDInaaTc=",
+      "url": "_framework\/_bin\/Blazui.Component.dll"
+    },
+    {
+      "hash": "sha256-INhDPFhYWf60eLRJEjs89B6ucNxk0Ww7kns3s1wKcCw=",
+      "url": "_framework\/_bin\/Blazui.Markdown.dll"
+    },
+    {
+      "hash": "sha256-g1174M295moi1CVaEFLO5i8A2MNJlsfu01L\/\/6Ne0Ig=",
+      "url": "_framework\/_bin\/Markdig.dll"
+    },
+    {
+      "hash": "sha256-rJEO\/MkGQOUr18evejTBoE9r6VojIf93Zmn9VKzD9PE=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Blazor.HttpClient.dll"
+    },
+    {
+      "hash": "sha256-mbZ9BLI8KDZomrTTeqNjnDFWTaozUjwSqtX\/kogOctM=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-a1Uxjg+ZBFx+YNOYxY6\/waNYZOlWDtSpymqouVuM1wY=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-rO+0yTL65JkZFj9jQamCjCAuV0r1R5t2+RxtqcJshk0=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-jzHgXWAvWMkKIGFjZoT84tbe72E+H7CvTr\/Dryh4QPs=",
+      "url": "_framework\/_bin\/Microsoft.Bcl.AsyncInterfaces.dll"
+    },
+    {
+      "hash": "sha256-+d5MVwK4dPMjOFWS\/A0G1Ypre2lM1xWx6jP9u9L+gGg=",
+      "url": "_framework\/_bin\/Microsoft.CSharp.dll"
+    },
+    {
+      "hash": "sha256-98uZYfvy\/WBeFcjpQguNbNxwTveoLyvE4VYCaAsMyvw=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-t\/pyxbDMRGcX1F28CnCIczdZ8EcGI0WvuzVdrNIuv+w=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-KURNo0g3oipy2nCzftltloJotg42uO0\/Wx83d7o8evc=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.FileExtensions.dll"
+    },
+    {
+      "hash": "sha256-HUKIan1llBpbZaz0CiCLgOK2hrSLUm6qOihkkjUGMDM=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-aXlMJ9WJl10z3P48gLvC1LtMSEMtBl4\/B4Cj7Ow977U=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-Wt3mFN3FyFhuWNEKd+\/tGBYkBDm1TztBxNoUmNKYVsU=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-b4eus2F93BogjVcaysM0ErKtXzRm3Wp1nkR2qFV07do=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.FileProviders.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-DDHVYkBN781h5cf26FBx4gCJ+U1\/i3IS505T5e5CkIY=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.FileProviders.Physical.dll"
+    },
+    {
+      "hash": "sha256-VDzRUGiHWFPrQ3LLELxEYXbEyvWTgFUmxteoIBpWc\/E=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.FileSystemGlobbing.dll"
+    },
+    {
+      "hash": "sha256-a71aWdQop2q69gBTsZRMIRwaQUQZZMJ98N5dm2K3f+M=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-6ZuMUlJOqjFzeacd0yNY+C1Q\/SXG02LiW7rPqMlf4RA=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-GFqkJmxwbf7WMrZ+MCRzaOADzSsVQ9tcMrMt19fsEeo=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-0ACKwHKkYNbvsypzH3gghBIuxTLDa+mtzKvDimvc794=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-e4mQQVutoMd9uhmE1v3abP6xagX6wM1NYy\/1Sea5hlQ=",
+      "url": "_framework\/_bin\/mscorlib.dll"
+    },
+    {
+      "hash": "sha256-2uX0mDh+fotyTuJqTWcHvIFmI7SFq16A8Oyp8zGS00A=",
+      "url": "_framework\/_bin\/System.ComponentModel.DataAnnotations.dll"
+    },
+    {
+      "hash": "sha256-A2xsHJ4E8+KgubdOV3rDsD3hk6muzkDp0mlhYtFKR14=",
+      "url": "_framework\/_bin\/System.Core.dll"
+    },
+    {
+      "hash": "sha256-mNPRNP1Ooxr4R\/CiguHBNHpSSuKaSWFpZWKEHrRBCMM=",
+      "url": "_framework\/_bin\/System.dll"
+    },
+    {
+      "hash": "sha256-YDdrNGLDBiAUjZy4V0NY3o9k9DUtEhcQzaetW0U\/RM8=",
+      "url": "_framework\/_bin\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-DLAZPsgDITK3IBB95pklST7PHH\/H\/MilXK\/L+X+N8nQ=",
+      "url": "_framework\/_bin\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-uYTm4LoJnlXfsmO26StowPTwk6QCkX7IuZz8iha+4rY=",
+      "url": "_framework\/_bin\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-WXbH\/ALcSrwR1HgvRxtFJAQ5\/KaAJkVvnqG8rbDgm0I=",
+      "url": "_framework\/_bin\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-B\/+Ot4BKlTU0Lv\/yLIZ1njo2Zf0SGidSINuh0BzBz9c=",
+      "url": "_framework\/_bin\/WebAssembly.Bindings.dll"
+    },
+    {
+      "hash": "sha256-j9CHA\/HhvAJ3fSTcl6yfBHGl\/OgfBfpme7vqboclABk=",
+      "url": "_framework\/_bin\/WebAssembly.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-80Q7rIpqLxXzuMWqtC+FCBNrG15kR\/r+ba08n2TpQO0=",
+      "url": "_framework\/wasm\/dotnet.3.2.0-preview2.20159.2.js"
+    },
+    {
+      "hash": "sha256-TwiabWTQ0R8CJeRzc59eU16DKfcf2p8PIvZsyWF7\/8M=",
+      "url": "_framework\/wasm\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-MLHdLkfFTg4QSXECyrV7KoY5xrSNtzBIlwrHcbW\/prE=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-Axa0DmuuB6r3NpyW4Bt3YrQfGeiiOwPsDTE1yKRKJfk=",
+      "url": "_framework\/blazor.boot.json"
+    }
+  ],
+  "version": "sep3ggcj"
+};
