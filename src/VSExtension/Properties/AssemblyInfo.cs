dc913766300c7877e127cdf47a38dcd702a56aba
maikebing <mysticboy@live.com>
2026-05-22 13:02:01 +0800

    refactor: rename VS extension to Element
    Remove the Blazui VS extension artifacts and project templates, replacing them with Element package metadata, icons, and template naming.

--- src/VSExtension/Properties/AssemblyInfo.cs
+++ src/VSExtension/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blazui.VSExtension")]
+[assembly: AssemblyTitle("Element.VSExtension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blazui.VSExtension")]
+[assembly: AssemblyProduct("Element.VSExtension")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
